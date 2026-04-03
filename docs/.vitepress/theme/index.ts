5eeb686bb33b6c2b35f18d88cfc7ae33cebcc08c
伊里奇 <ilyichbc@outlook.com>
2026-04-03 10:47:16 +0800

    Add files via upload

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,20 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import './style.css'
+import { useData, useRoute } from 'vitepress'
+import "vitepress-markdown-timeline/dist/theme/index.css"
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  },
+    setup() {
+    }
+} satisfies Theme
